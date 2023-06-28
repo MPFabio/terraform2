9c89ce2c2d0341deee730f87a0e9404ced50751a
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-28 15:57:51 +0200

    Update main.tf

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -3,7 +3,7 @@ variable "location" {}
 variable "vnet_id" {}
 
 resource "azurerm_network_security_group" "example" {
-  name                = "my-nsg"
+  name                = "fabio-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
 
