37e6c8bc8ca65ebe5cabe53149b1e910bcbff865
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-28 15:58:10 +0200

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -4,14 +4,14 @@ variable "vnet_cidr" {}
 variable "subnet_cidr" {}
 
 resource "azurerm_virtual_network" "example" {
-  name                = "my-vnet"
+  name                = "fabio-vnet"
   address_space       = [var.vnet_cidr]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
+  name                 = "fabio-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = [var.subnet_cidr]
